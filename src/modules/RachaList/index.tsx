731e02c071e4d798094fb795034e63ea0efbb2b7
Daniel Abrante <danielabrante@protonmail.com>
2025-02-24 18:20:01 -0300

    feat: add input to add player and show players list

--- src/modules/RachaList/index.tsx
+++ src/modules/RachaList/index.tsx
@@ -1,15 +1,45 @@
+import { useState } from "react";
 import S from "./RachaList.module.css"
 
 export function RachaList() {
+    const [newPlayerInput, setNewPlayerInput] = useState<string>('');
+    const [playersList, setPlayersList] = useState<string[]>([]);
+
+    const renamePlayer = (index: number, value: string) => {
+        const updatedPlayersList = [...playersList]
+
+        updatedPlayersList[index] = value;
+
+        setPlayersList(updatedPlayersList);
+    }
+
+    const addNewPlayer = () => {
+        if (!newPlayerInput || newPlayerInput.trim() === '') return;
+
+        setNewPlayerInput('')
+
+        setPlayersList([...playersList, newPlayerInput])
+    }
+
     return (
         <article className={S.RachaListContainer}>
             <section>
                 <h1>Lista do Racha</h1>
+
+                <ul>
+                    {
+                        playersList.map((player, index) => (
+                            <li>
+                                <input type="text" value={player} key={index} onChange={(e) => renamePlayer(index, e.target.value)} />
+                            </li>
+                        ))
+                    }
+                </ul>
             </section>
 
             <section className={S.RachaListForm}>
-                <input type="text" placeholder="Jogador(a)" />
-                <button type="button">Adicionar</button>
+                <input type="text" placeholder="Jogador(a)" value={newPlayerInput} onChange={(e) => setNewPlayerInput(e.target.value)} autoFocus />
+                <button type="button" onClick={addNewPlayer}>Adicionar</button>
             </section>
         </article>
     );
